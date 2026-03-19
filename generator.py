6987a1d65e9c1025632852c0a9ab2e4c84fa6f1a
lamviechappy <116244519+lamviechappy@users.noreply.github.com>
2026-03-19 09:38:17 +0700

    cleanning

--- generator.py
+++ generator.py
@@ -8,7 +8,7 @@ from models import Model
 from moshi.models import loaders
 from tokenizers.processors import TemplateProcessing
 from transformers import AutoTokenizer
-from watermarking import CSM_1B_GH_WATERMARK, load_watermarker, watermark
+from backups.watermarking import CSM_1B_GH_WATERMARK, load_watermarker, watermark
 
 
 import torch
